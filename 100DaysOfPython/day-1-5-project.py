2266625e9b74215ddd1ef33ddaff73c6eb2768b7
grahamhartle <ghartle@me.com>
2021-09-13 08:47:04 +1000

    updates

--- 100DaysOfPython/day-1-5-project.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator")
-city = input("What city did you grow up in?\n")
-pet_name = input("What is your pet's name?\n")
-print(f"The name of your band is {city} {pet_name}")
