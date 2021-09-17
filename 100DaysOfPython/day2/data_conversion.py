91b5230b45eaf4ca76a2ac34bc66cb6f0708f634
grahamhartle <ghartle@me.com>
2021-09-17 17:52:08 +1000

    file updates

---
+++ 100DaysOfPython/day2/data_conversion.py
@@ -0,0 +1,6 @@
+num = input("Enter a 2 digit number: ")
+
+first_num = int(num[0])
+second_num = int(num[1])
+
+print(f"{first_num} + {second_num} = {first_num + second_num}")
--- test.py
+++ test.py
@@ -1,11 +1,28 @@
-def xyz(a):
 
-    if a == 33:
-        return "No"
-    elif a <= 30 or a >= 40:
-        return "No"
-    else:
-        return "Yes"
+# mylist = [1,1,3,4,5,5,5,6]
+# count = 0
+# for i in range(len(mylist)):
+#     for k in range(len(mylist)):
+#         if i == k :
+#             continue
+#         elif mylist[i] == mylist[k]:
+#             del mylist[k]
+#             mylist.append(0)
+#             count = count + 1
 
+# print(count)
+# print(mylist)
 
-print(xyz(31))
+# ls = [1,1,3,4,5,5,5,6]
+# result = []
+# for item in ls[:]:
+#     if item not in result:
+#         result.append(item)
+
+# print(result)
+
+mylist = [1,1,3,4,5,5,5,6]
+myset = set(mylist)
+
+print(len(mylist) - len(myset))
+
